f52b2701fce7b0cc38381f44baf7ea53d894cd97
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-01-16 13:30:32 +0100

    Todas las carpetas de Flutter

---
+++ curso_widget/android/app/src/main/kotlin/com/example/curso_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.curso_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ poryecto_pokemon/android/app/src/main/kotlin/com/example/poryecto_pokemon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poryecto_pokemon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
