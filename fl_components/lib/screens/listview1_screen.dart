9b94e85677a0ae265191e98cb4cd810176d13f63
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-01-17 10:48:18 +0100

    Avance en clase curso de Flutter

---
+++ fl_components/lib/screens/listview1_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+
+  final options = const['Megaman','Metal Gear','Super Smash','Final Fantasy'];
+
+  const Listview1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Tipo 1'),
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+
+            ...options.map(
+              (game) => ListTile (
+                title: Text(game),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              )
+            ).toList(),
+          ],
+        )
+      ),
+    );
+  }
+
+}
