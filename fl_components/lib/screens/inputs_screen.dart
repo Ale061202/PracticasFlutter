9625fb2cce9363a202663c3f7d5c2b9a66f18bbf
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-01 17:41:33 +0100

    input screen en casa

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
 
 class InputsScreen extends StatelessWidget {
 const InputsScreen({ Key? key }) : super(key: key);
@@ -9,20 +9,27 @@ const InputsScreen({ Key? key }) : super(key: key);
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inputs y Forms'),
+        title: const Text('Inputs y Forms'),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-              )
-            ]
+            children: const [
+
+              CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario', helperText: 'Nombre del usuario',),
+              SizedBox(height: 30,),
+
+              CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario', helperText: 'Apellido del usuario',),
+              SizedBox(height: 30,),
+
+              CustomInputField(labelText: 'Correo', hintText: 'Correo del usuario', helperText: 'Correo del usuario',),
+              SizedBox(height: 30,),
+
+            ],
           ),
-        )
-      ),
+        ),
+      )
     );
   }
 }
